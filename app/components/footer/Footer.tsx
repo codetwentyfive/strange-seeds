4f81ad6c81ac95da285d3ffc9c4b22f1a20a3875
code25 <chingisenkhbaatar@gmail.com>
2024-10-18 21:32:49 +0200

    footer be footing

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex flex-col md:flex-row items-center justify-between p-4 bg-background text-sm">
-      <p>&copy; {new Date().getFullYear()} Your Band Name. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} the Strange Seeds. All rights reserved.</p>
       <div className="flex space-x-4 mt-2 md:mt-0">
         <Link href="/imprint">Imprint</Link>
         <Link href="/privacy">Privacy Policy</Link>
