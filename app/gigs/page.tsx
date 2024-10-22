7ac593ae77cadde3800a66e33b2bc03b0b5d497f
code25 <chingisenkhbaatar@gmail.com>
2024-10-22 23:08:54 +0200

    SEO2

--- app/gigs/page.tsx
+++ app/gigs/page.tsx
@@ -1,13 +1,24 @@
+import { useState, useEffect } from 'react';
 import Header from "../components/header/Header";
 import UpcomingGigs from "../components/gigs/UpcomingGigs";
 import Footer from "../components/footer/Footer";
+import { Gig } from '../components/types/gig';
 
 export default function GigsPage() {
+  const [gigs, setGigs] = useState<Gig[]>([]);
+
+  useEffect(() => {
+    fetch('/api/gigs/upcoming')
+      .then(response => response.json())
+      .then(data => setGigs(data))
+      .catch(error => console.error('Error fetching gigs:', error));
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
-        <UpcomingGigs />
+        <UpcomingGigs gigs={gigs} />
       </main>
       <Footer />
     </div>
